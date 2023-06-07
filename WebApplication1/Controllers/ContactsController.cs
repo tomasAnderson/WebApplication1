eaf04ae76a639b6a5fa47be68588a8abb14d9e4a
mr. Anderson <thomasandreson15514@gmail.com>
2023-06-08 01:39:28 +0300

    init

---
+++ WebApplication1/Controllers/ContactsController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers;
+
+public class ContactsController : Controller
+{
+    // GET
+    public IActionResult Index()
+    {
+        return View();
+    }
+    [HttpPost]
+    public IActionResult Check(ContactModel contactModel)
+    {
+        if (ModelState.IsValid)
+        {
+            return Redirect("/");
+        }
+        return View("Index");
+    }
+    public void her()
+    {
+    }
+    public void her(string her)
+    {
+    }
+}
---
+++ WebApplication1/Models/ContactModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.Models;
+
+public class ContactModel
+{
+    [Display(Name = "Введите имя")]
+    [Required(ErrorMessage = "Это поле обязательно к заполнению")]
+    public string Name { get; set; }
+    [Display(Name = "Введите фамилию")]
+    public string SurnameName { get; set; }
+    [Display(Name = "Введите возраст")]
+    public int Age { get; set; }
+    [Display(Name = "Введите почту")]
+    public string Email { get; set; }
+    [Display(Name = "Введите сообщение")]
+    public string Message { get; set; }
+    [Display(Name = "Введите пароль")]
+    public string Password { get; set; }
+}
